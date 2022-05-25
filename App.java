c6f24c7d69e235b15a3e6aeff34fc5eb61a4604e
aconkli9 <aconkli9@ford.com>
2022-05-25 12:29:46 -0400

    Add solution

--- App.java
+++ App.java
@@ -1,6 +1,18 @@
 class App {
-  public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+  public static void main(String args[]) {
+    int VIN = 9_999_999;
+    String makeModel = "Ford Mustang";
+    String color = "White";
+    boolean towPackage = false;
+    int mileage = 60000;
+    short price = 15000;
+    char quality = 'A';
+    System.out.println(VIN);
+    System.out.println(makeModel);
+    System.out.println(color);
+    System.out.println(towPackage);
+    System.out.println(mileage);
+    System.out.println(price);
+    System.out.println(quality);
   }
 }
